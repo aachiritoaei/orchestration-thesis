161fcb441a40d582c43d6832c17c21101669b974
aachiritoaei <achirialex@gmail.com>
2018-04-23 23:11:51 +0300

    Added Zuul for api gateway.

--- new-api-gateway/src/main/java/app/Application.java
+++ new-api-gateway/src/main/java/app/Application.java
@@ -2,10 +2,10 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.sidecar.EnableSidecar;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableSidecar
+@EnableZuulProxy
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
