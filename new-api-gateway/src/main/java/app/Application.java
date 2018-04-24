c0a22376cf7acea49bca4f89b5f372e1e762cc8a
aachiritoaei <achirialex@gmail.com>
2018-04-24 12:23:54 +0300

    Finish gateway, discovery, io and meta.

--- new-api-gateway/src/main/java/app/Application.java
+++ new-api-gateway/src/main/java/app/Application.java
@@ -2,10 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
