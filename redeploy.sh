3282859c1e8ca309028f7e250cf35afa66f05fae
aachiritoaei <achirialex@gmail.com>
2018-05-31 10:13:59 +0300

    Containrized discovery, gateway, meta.

---
+++ redeploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+docker stop $(docker ps -aq) 
+docker rm $(docker ps -aq)
+docker rmi $(docker images -q)
+
+cd new-api-gateway
+mvn clean package
+docker build -t new-api-gateway .
+
+cd ..
+
+cd new-discovery-service
+mvn clean package
+docker build -t new-discovery-server .
+
+cd ..
+
+cd new-meta-server
+mvn clean package
+docker build -t new-meta-server .
+
+cd ..
+
+docker-compose up
